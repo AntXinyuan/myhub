0b2f5e70a13611d8e4ad8c65a69c52213658d013
AntXinyuan <ant_xinyuan@163.com>
2018-12-08 21:53:24 +0800

    Create a idea git file

---
+++ src/main/java/Main.java
@@ -0,0 +1,33 @@
+import okhttp3.*;
+
+import java.io.IOException;
+
+public class Main {
+    public static void sendHttpRequest(String address,okhttp3.Callback callback){
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+    public static void main(String args[]){
+        String url="http://guolin.tech/api/china";
+        OkHttpClient client=new OkHttpClient();
+        Request request=new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                System.out.println("Error!");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String result=response.body().string();
+                System.out.println(result);
+            }
+        });
+    }
+
+
+}
+
+
